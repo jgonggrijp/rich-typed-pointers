481f7c8045103ae91a221888387773a199af9a5a
Julian Gonggrijp <j.gonggrijp@gmail.com>
2013-09-25 17:44:39 +0200

    Refocus the basic usage example on really basic usage

--- example/basic_usage.cpp
+++ example/basic_usage.cpp
@@ -15,6 +15,13 @@ rtp::weak_ptr<int> ptr_min (rtp::weak_ptr<int> left, rtp::weak_ptr<int> right) {
     return left;
 }
 
+// generic version of the above
+template <class Ptr>
+Ptr ptr_min_generic (Ptr left, Ptr right) {
+    if (*right < *left) return right;
+    return left;
+}
+
 // functions return newly created pointers as owner_ptr
 rtp::owner_ptr<int> givemeapointer (int value) {
     // rtp::make is the only way to create a rich-typed pointer
@@ -23,27 +30,20 @@ rtp::owner_ptr<int> givemeapointer (int value) {
 
 int main ( ) {
     // you can declare all local rich-typed pointers using auto
-    auto test1 = givemeapointer(4);
+    auto test1 = rtp::make<int>(4);
+    auto test2 = givemeapointer(4);
 
-    // copy from an owner_ptr requires explicit type or a typecast
-    // rtp::weak is a convenience function that performs the typecast
-    auto test2 = rtp::weak<int>(test1);
+    mutatesomeintptr(test1);  // test1 automatically casted to weak_ptr
+    cout << *test1 << ' ' << *test2 << endl;  // 9 4
 
-    cout << *test1 << ' ' << *test2 << endl;  // 4 4
+    auto test3 = ptr_min(test1, test2);
+    cout << *test3 << endl;  // 4
 
-    mutatesomeintptr(test1);
-    cout << *test1 << ' ' << *test2 << endl;  // 9 9
-
-    mutatesomeintptr(test2);
-    cout << *test1 << ' ' << *test2 << endl;  // 14 14
-
-    // copy from a weak_ptr allows auto type inference
-    auto test3 = test2;
-
-    auto test4 = rtp::make<int>(4);
-    auto test5 = rtp::weak<int>(test4);
-    auto test6 = ptr_min(test1, test5);  // no cast needed here
-    cout << *test6 << endl;  // 4
+    // due to a limitation in the C++ type system the following won't compile:
+    // auto test4 = ptr_min_generic(test1, test2);
+    // this can be solved with the convenience cast function `weak`:
+    auto test4 = ptr_min_generic(rtp::weak(test1), rtp::weak(test2));
+    cout << *test4 << endl;  // 4
 
     return 0;  // memory is automatically reclaimed
 }
